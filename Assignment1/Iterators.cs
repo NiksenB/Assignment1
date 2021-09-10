ecde2b408c2e0b95f795e7f12d1c84b8a8566b76
Nikoline <nibu@itu.dk>
2021-09-10 13:29:48 +0200

    get comments

--- Assignment1/Iterators.cs
+++ Assignment1/Iterators.cs
@@ -5,11 +5,14 @@ namespace Assignment1
 {
     public static class Iterators
     {
+        //Flatten takes as argument a stream of a stream of T's. It should return a stream of T's.
         public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items)
         {
             throw new NotImplementedException();
         }
 
+        /* Filter takes as arguments a stream of T's and a function which returns true or false when 
+        applied to an instance of T. It returns a stream of only the Ts where the predicate returns true. */
         public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate)
         {
             throw new NotImplementedException();
